a8072995de07331c7c48e8a6b64cb6b43efe4940
Sean398 <guohaochuan5461@163.com>
2021-02-08 20:22:04 +0800

    enviroment_setup

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app} = require('electron');
+const isDev = require('electron-is-dev');
+const { join } = require('path');
+const AppWindow = require('./src/AppWindow');
+
+let mainWindow;
+
+app.on('ready', () => {
+    const urlLocation = isDev
+        ? 'http://localhost:3000'
+        : `file://${join(__dirname, './build/index.html')}`;
+        
+
+    mainWindow = new AppWindow(
+        {
+            width: 1024,
+            height: 700
+        },
+        urlLocation
+    );
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'antd/dist/antd.css';
+// import Router from './router';
+
+const App = () => {
+    return (
+        <div className="App container-fluid">
+            {/* <Router />x */}
+        </div>
+    );
+};
+
+export default App;
